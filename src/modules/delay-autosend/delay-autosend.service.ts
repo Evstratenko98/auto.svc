56f1781ecd73aae6b31ba6a79fb60f9bc7eacf9a
stas.evstratenko <stas.evstratenko@sravni.ru>
2026-02-03 10:23:56 +0300

    CF-4482

--- src/modules/delay-autosend/delay-autosend.service.ts
+++ src/modules/delay-autosend/delay-autosend.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
 import { DelayAutosendRequestDto } from './dto/delay-autosend.request.dto';
@@ -7,32 +7,41 @@ import { QUEUE_TITLE } from './delay-autosend.constants';
 @Injectable()
 export class DelayAutosendService {
   private DEFAULT_DELAY_MINUTES = 30;
+  private readonly logger = new Logger(DelayAutosendService.name);
 
   constructor(@InjectQueue('delay-autosend') private readonly delayAutosendQueue: Queue) {}
 
   async scheduleAutosend(delayAutosendRequestDto: DelayAutosendRequestDto) {
-    const { userId, offers, source, delay } = delayAutosendRequestDto;
-    const jobId = `delayautosend_${userId}`;
-    const delayMinutes = delay ?? this.DEFAULT_DELAY_MINUTES;
-    const delayMs = delayMinutes * 60 * 1000;
+    try {
+      const { userId, offers, source, delay } = delayAutosendRequestDto;
+      const jobId = `delayautosend_${userId}`;
+      const delayMinutes = delay ?? this.DEFAULT_DELAY_MINUTES;
+      const delayMs = delayMinutes * 60 * 1000;
 
-    const existingJob = await this.delayAutosendQueue.getJob(jobId);
-    if (existingJob) {
-      await existingJob.remove();
-    }
+      const existingJob = await this.delayAutosendQueue.getJob(jobId);
+      if (existingJob) {
+        await existingJob.remove();
+      }
 
-    await this.delayAutosendQueue.add(
-      QUEUE_TITLE,
-      { userId, offers, source },
-      {
+      await this.delayAutosendQueue.add(
+        QUEUE_TITLE,
+        { userId, offers, source },
+        {
+          jobId,
+          delay: delayMs,
+        },
+      );
+
+      return {
+        success: true,
         jobId,
-        delay: delayMs,
-      },
-    );
-
-    return {
-      success: true,
-      jobId,
-    };
+      };
+    } catch (error) {
+      this.logger.error({ error });
+      return {
+        success: false,
+        jobId: null,
+      };
+    }
   }
 }
